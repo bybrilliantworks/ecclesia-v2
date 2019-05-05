261ea23c5be498530fdce5acf252b3ef3aa5ece8
Babajide Owosakin <babajide.owosakin@andela.com>
2019-05-05 08:46:34 +0100

    feat(import): modify members import

--- app/Imports/MembersImport.php
+++ app/Imports/MembersImport.php
@@ -3,11 +3,16 @@
 namespace App\Imports;
 
 use App\Member;
+use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\SkipsFailures;
+use Maatwebsite\Excel\Concerns\SkipsOnError;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Validators\Failure;
 
-class MembersImport implements ToModel, WithHeadingRow
+class MembersImport implements ToModel, WithHeadingRow, SkipsOnError
 {
+    use Importable, SkipsFailures;
     /**
      * @param array $row
      *
@@ -27,21 +32,32 @@ class MembersImport implements ToModel, WithHeadingRow
         }
 
         return new Member([
-            'name' => $row['surname'] . " " . $row['first_name'],
+            'name' => $row['name'] ? $row['name'] : $row['surname'] . " " . $row['first_name'],
             'surname' => $row['surname'],
             'first_name' => $row['first_name'],
-            'gender' => $row['sex'] ? $genderMap[$row['sex']] : 'other',
+            'gender' => $row['gender'] ? $genderMap[$row['gender']] : 'other',
             'email' => strtolower($row['email']),
             'church_id' => auth()->user()->church_id,
             'address' => $row['address'] ? $row['address'] : 'Not Available',
-            'neighbourhood' => $row['area'],
+            'neighbourhood' => $row['neighbourhood'],
             'mobile_number' => $row['mobile_number'] ? $row['mobile_number'] : 'Not Available',
             'state' => $row['state'],
-            'marital_status' => $row['relationship_status'] ? $row['relationship_status'] : 'Not Available',
+            'marital_status' => $row['marital_status'] ? $row['marital_status'] : 'Not Available',
             'employment_status' => $employmentStatus,
+            'membership_number' => $row['membership_number'],
             'occupation' => $row['occupation'],
             'created_at' => \Carbon\Carbon::now(),
             'updated_at' => \Carbon\Carbon::now(),
         ]);
+    }
+
+    public function onFailure(Failure...$failures)
+    {
+        // Handle the failures how you'd like.
+    }
+
+    public function onError(\Throwable $e)
+    {
+        // Handle the exception how you'd like.
     }
 }
